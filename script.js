515e4a0b0d2f409f7a914f3e4b090e43891e0f15
Surjeet Kumar <157746293+surjeetkumar8006@users.noreply.github.com>
2025-09-22 23:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading spaces to handle cases like ' Hello World!'
+  s = s.trimStart();
+
+  // Find the index of the first space
+  let spaceIndex = s.indexOf(' ');
+
+  // If no space found, return the whole string
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  // Return substring from start to the first space
+  return s.substring(0, spaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// Example tests
+// console.log(firstWord('see and stop'));    // 'see'
+// console.log(firstWord(' Hello World!'));   // 'Hello'
+// console.log(firstWord('12345'));            // '12345'
+// console.log(firstWord(''));                  // ''
